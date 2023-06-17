0f79d071e66c24dcf3620faf04d9c20c41aa1cfb
Bruce CHEN <805899926@qq.com>
2023-06-17 12:45:14 +0800

    #

--- service/sunny-cloud-system/sunny-cloud-system-portal/src/main/java/com/sunny/cloud/system/portal/SystemApplication.java
+++ service/sunny-cloud-system/sunny-cloud-system-portal/src/main/java/com/sunny/cloud/system/portal/SystemApplication.java
@@ -3,7 +3,7 @@ package com.sunny.cloud.system.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sunny.cloud.system.*")
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
