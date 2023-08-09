e5177d27ce9c035c794e66ea1f70839bb51c18d3
Bruce CHEN <805899926@qq.com>
2023-08-09 23:28:11 +0800

    #

--- service/sunny-cloud-auth/sunny-cloud-auth-portal/src/main/java/com/sunny/cloud/auth/portal/AuthApplication.java
+++ service/sunny-cloud-auth/sunny-cloud-auth-portal/src/main/java/com/sunny/cloud/auth/portal/AuthApplication.java
@@ -1,16 +1,13 @@
-package com.sunny.cloud.system.portal;
+package com.sunny.cloud.auth.portal;
 
-import com.sunny.cloud.system.core.mapper.DictMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
-@MapperScan(basePackageClasses = DictMapper.class)
 @SpringBootApplication(scanBasePackages = "com.sunny.cloud.auth.*")
-public class SystemApplication {
+public class AuthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
