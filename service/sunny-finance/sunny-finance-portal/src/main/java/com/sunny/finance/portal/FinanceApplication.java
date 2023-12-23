74ca2e2ea7eaa1ba680d9b9c089102371ac486bd
Bruce CHEN <805899926@qq.com>
2023-12-23 17:00:05 +0800

    # use spring cloud kubernetes config server

--- service/sunny-finance/sunny-finance-portal/src/main/java/com/sunny/finance/portal/FinanceApplication.java
+++ service/sunny-finance/sunny-finance-portal/src/main/java/com/sunny/finance/portal/FinanceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication(scanBasePackages = "com.sunny.finance.*")
 public class FinanceApplication {
     public static void main(String[] args) {
