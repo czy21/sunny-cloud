0278e57e39f63a606e48f3a62fe3ee12098f3637
Bruce CHEN <805899926@qq.com>
2023-12-23 12:18:21 +0800

    # add finance service

---
+++ service/sunny-finance/sunny-finance-portal/src/main/java/com/sunny/finance/portal/FinanceApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.finance.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy(exposeProxy = true)
+@SpringBootApplication(scanBasePackages = "com.sunny.finance.*")
+public class FinanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceApplication.class, args);
+    }
+}
