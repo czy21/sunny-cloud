d777e1a276940a4d8bdd94f06b266165c7a53e2d
Bruce CHEN <805899926@qq.com>
2023-12-30 16:16:00 +0800

    #

--- service/sunny-finance/sunny-finance-portal/src/main/java/com/sunny/finance/portal/FinanceApplication.java
+++ service/sunny-finance/sunny-finance-portal/src/main/java/com/sunny/finance/portal/FinanceApplication.java
@@ -1,10 +1,13 @@
 package com.sunny.finance.portal;
 
+import com.sunny.finance.core.client.ProductClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication(scanBasePackages = "com.sunny.finance.*")
+@EnableFeignClients(basePackageClasses = ProductClient.class)
 public class FinanceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinanceApplication.class, args);
