4f7a090022f5845e8d776b70c797ba9309f43555
Bruce Chen <805899926@qq.com>
2024-04-23 16:43:56 +0800

    #

--- service/sunny-auth/sunny-auth-portal/src/main/java/com/sunny/auth/portal/controller/AuthController.java
+++ service/sunny-auth/sunny-auth-portal/src/main/java/com/sunny/auth/portal/controller/AuthController.java
@@ -1,13 +1,27 @@
 package com.sunny.auth.portal.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "认证API")
 @RestController
-@RequestMapping(path = "dict")
 public class AuthController  {
 
+    @GetMapping("/")
+    public String index( @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient, @AuthenticationPrincipal OAuth2User oauth2User) {
 
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "test";
+    }
 }
