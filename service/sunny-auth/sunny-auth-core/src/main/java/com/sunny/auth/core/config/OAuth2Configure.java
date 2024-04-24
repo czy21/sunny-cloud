b7168b797f98a02448cea7c6b80bd64fede0acb6
Bruce Chen <805899926@qq.com>
2024-04-24 11:24:42 +0800

    #

--- service/sunny-auth/sunny-auth-core/src/main/java/com/sunny/auth/core/config/OAuth2Configure.java
+++ service/sunny-auth/sunny-auth-core/src/main/java/com/sunny/auth/core/config/OAuth2Configure.java
@@ -1,24 +1,41 @@
 package com.sunny.auth.core.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@AllArgsConstructor
 public class OAuth2Configure {
 
-
     JdbcTemplate jdbcTemplate;
-
-    public OAuth2Configure(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @Bean
     OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
         return new JdbcOAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepository);
     }
+
+    @Bean
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(t ->
+                t.anyRequest().permitAll()
+        );
+//        http.sessionManagement(t->{
+//            t.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        });
+        http.oauth2Login(t -> {
+        });
+        http.oauth2Client(t -> {
+
+        });
+        return http.build();
+    }
+
 }
