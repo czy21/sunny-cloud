fd6d75761888650113d39abc9b8c91c20d8cad13
Bruce Chen <805899926@qq.com>
2024-04-25 16:47:45 +0800

    #

---
+++ service/sunny-auth/sunny-auth-core/src/main/java/com/sunny/auth/core/config/CustomUserDetailService.java
@@ -0,0 +1,14 @@
+package com.sunny.auth.core.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
