767ff6d512387938d067f09e07b6987a06530992
Bruce Chen <805899926@qq.com>
2024-04-26 13:07:36 +0800

    #

--- service/sunny-auth/sunny-auth-core/src/main/java/com/sunny/auth/core/config/OAuth2Configure.java
+++ service/sunny-auth/sunny-auth-core/src/main/java/com/sunny/auth/core/config/OAuth2Configure.java
@@ -5,8 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
 
 @Configuration
 @AllArgsConstructor
@@ -14,29 +30,70 @@ public class OAuth2Configure {
 
     JdbcTemplate jdbcTemplate;
 
+    @Bean
+    OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
+        return new JdbcOAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepository);
+    }
+
 //    @Bean
-//    OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
-//        return new JdbcOAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepository);
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+//        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+//        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
+//        http.exceptionHandling(t ->
+//                t.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+//        );
+//        return http.build();
 //    }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailService();
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient loginClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("login-client")
+                .clientSecret("{noop}openid-connect")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/login-client")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("messaging-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("message:read")
+                .scope("message:write")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(loginClient, registeredClient);
     }
 
     @Bean
-    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(t -> {
-                    t.anyRequest().authenticated();
-                }
+    public SecurityFilterChain standardSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(t ->
+                t.anyRequest().authenticated()
         );
-        http.httpBasic(t->{
+        http.formLogin(t -> {
 
         });
-        http.formLogin(t->{
-
-        });
+        http.sessionManagement(AbstractHttpConfigurer::disable);
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+
+    }
 }
