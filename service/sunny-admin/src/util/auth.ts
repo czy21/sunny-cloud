d4fced77f588caa58bb10bc175b928146c22562e
Bruce CHEN <805899926@qq.com>
2024-05-05 16:05:47 +0800

    #

--- service/sunny-admin/src/util/auth.ts
+++ service/sunny-admin/src/util/auth.ts
@@ -2,17 +2,14 @@ import Cookies from "js-cookie";
 
 const tokenKey = 'token'
 
-export function getToken(): string | null {
-    Cookies.get(tokenKey);
-    return localStorage.getItem(tokenKey)
+export function getToken() {
+    return Cookies.get(tokenKey)
 }
 
 export function setToken(value: string) {
     Cookies.set(tokenKey, value, {domain: '.' + window.location.hostname.split('.').slice(-2).join('.')});
-    return localStorage.setItem(tokenKey, value);
 }
 
 export function delToken() {
     Cookies.remove(tokenKey, {domain: '.' + window.location.hostname.split('.').slice(-2).join('.')});
-    return localStorage.removeItem(tokenKey);
 }
