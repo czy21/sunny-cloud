b8149c4b8c09b895895891f6435182ba80d35f67
Bruce CHEN <805899926@qq.com>
2024-06-01 16:21:36 +0800

    # test property read order

--- service/sunny-example/sunny-example-portal/src/main/java/com/sunny/example/portal/controller/ExampleController.java
+++ service/sunny-example/sunny-example-portal/src/main/java/com/sunny/example/portal/controller/ExampleController.java
@@ -1,11 +1,11 @@
 package com.sunny.example.portal.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "finance")
 public class ExampleController {
-
 
 }
