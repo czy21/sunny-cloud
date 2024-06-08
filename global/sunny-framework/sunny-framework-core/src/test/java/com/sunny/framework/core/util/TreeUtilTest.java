1c5bbc1a666e16b2a7c8d90b5d6fc41104c4f38f
Bruce CHEN <805899926@qq.com>
2024-06-08 16:14:10 +0800

    #

--- global/sunny-framework/sunny-framework-core/src/test/java/com/sunny/framework/core/util/TreeUtilTest.java
+++ global/sunny-framework/sunny-framework-core/src/test/java/com/sunny/framework/core/util/TreeUtilTest.java
@@ -18,8 +18,12 @@ public class TreeUtilTest {
         URL url = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX + "tree.json");
         List<SimpleItemModel<String>> items = objectMapper.readValue(url, new TypeReference<List<SimpleItemModel<String>>>() {
         });
-        TreeUtil.assemble(items);
-        List<SimpleItemModel<String>> tree = TreeUtil.getTree(items, t -> t.getParentValue() == null);
+        List<SimpleItemModel<String>> tree = TreeUtil.getTree(items,
+                t -> t.getParentValue() == null,
+                t -> {
+                    t.setParentIds(TreeUtil.getParentIds(items, t));
+                }
+        );
         System.out.println();
     }
 }
