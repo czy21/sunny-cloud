7903cc3593ccac8ec9063c28a53f2c20ca37eb8c
Bruce CHEN <805899926@qq.com>
2024-07-23 18:43:10 +0800

    #

--- global/sunny-framework-test/src/test/java/com/sunny/framework/test/config/MybatisJdbcConfigure.java
+++ global/sunny-framework-test/src/test/java/com/sunny/framework/test/config/MybatisJdbcConfigure.java
@@ -1,15 +1,12 @@
 package com.sunny.framework.test.config;
 
 import com.sunny.framework.test.mapper.UserMapper;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.data.jdbc.repository.config.MyBatisJdbcConfiguration;
 
-@Configuration
-@EnableJdbcRepositories(basePackageClasses = UserMapper.class)
-@Import(MyBatisJdbcConfiguration.class)
-public class TestConfigure {
+
+public class MybatisJdbcConfigure {
 
 
 }
--- global/sunny-framework-test/src/test/java/com/sunny/framework/test/db/MybatisJdbcTest.java
+++ global/sunny-framework-test/src/test/java/com/sunny/framework/test/db/MybatisJdbcTest.java
@@ -5,15 +5,20 @@ import com.sunny.framework.test.model.po.UserPO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.jdbc.repository.config.MyBatisJdbcConfiguration;
 
 @SpringBootTest
-public class DBTest {
+@EnableJdbcRepositories(basePackageClasses = UserMapper.class)
+@Import(MyBatisJdbcConfiguration.class)
+public class MybatisJdbcTest {
 
     @Autowired
     UserMapper userMapper;
 
     @Test
-    public void testMybatisInsert() {
+    public void testInsert() {
         UserPO userPO = new UserPO();
         userPO.setName("你好");
         UserPO inserted = userMapper.save(userPO);
