5b035f177ae87a3353f480da881a224e03ed9912
Bruce CHEN <805899926@qq.com>
2024-07-25 16:52:19 +0800

    #

--- service/sunny-admin/vite.config.ts
+++ service/sunny-admin/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
         port: 5174,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8082/api/',
+                target: 'http://sunny-gateway.czy21-internal.com/',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
             },
