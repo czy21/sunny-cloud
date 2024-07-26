f7b6bce5329403d3307b5b91b6f292493ce8e735
Bruce CHEN <805899926@qq.com>
2024-07-26 16:24:12 +0800

    # improve framework-js

--- service/sunny-auth-web/src/main.ts
+++ service/sunny-auth-web/src/main.ts
@@ -4,7 +4,7 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from "@/router";
 import store from "@/store"
-import util from "@/util"
+import util from "@g/util"
 
 const app = createApp(App)
 app.use(ElementPlus, {size: 'mini'} as any)
