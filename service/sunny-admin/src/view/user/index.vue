4b0d08e0763790f75dcd8cf2e2dd55b89b3c9c11
Bruce CHEN <805899926@qq.com>
2024-07-29 10:42:36 +0800

    #

--- service/sunny-admin/src/view/user/index.vue
+++ service/sunny-admin/src/view/user/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <el-button @click="handleSearch">查询</el-button>
+  <el-form :inline="true" :model="queryFormModel">
+    <el-form-item label="姓名">
+      <el-input v-model="queryFormModel.name" placeholder="请输入姓名" clearable/>
+    </el-form-item>
+    <el-form-item label="手机号">
+      <el-input v-model="queryFormModel.phone" placeholder="请输入手机号" clearable/>
+    </el-form-item>
+    <el-form-item label="邮箱">
+      <el-input v-model="queryFormModel.email" placeholder="请输入邮箱" clearable/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="handleSearch">查询</el-button>
+    </el-form-item>
+  </el-form>
   <el-table :data="list" border style="width: 100%">
     <el-table-column prop="username" label="账号"/>
     <el-table-column prop="name" label="姓名"/>
+    <el-table-column prop="phone" label="手机号"/>
     <el-table-column prop="email" label="邮箱"/>
     <el-table-column prop="address" label="地址"/>
   </el-table>
@@ -13,28 +27,37 @@
       :page-sizes="[50, 100, 150, 200]"
       :size="page.totalPage"
       :total="page.total"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
+      @size-change="handlePageSizeChange"
+      @current-change="handlePageIndexChange"
   />
 </template>
 <script setup lang="ts">
 import api from '@/api';
+import {FormInstance} from 'element-plus';
 import _ from 'lodash'
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 
+const queryFormRef = ref<FormInstance>()
+const queryFormModel = reactive({
+  name: "",
+  phone: "",
+  email: ""
+})
 const page: any = ref({})
 const list: any = ref([])
-const handleSearch = (param = {}) => {
-  api.post("sys/user/page", param).then((t: any) => {
+const handleSearch = (query = {}) => {
+  api.post("sys/user/page", {..._.omit(page.value, ["total", "totalPage"]), ...queryFormModel}).then((t: any) => {
     page.value = _.omit(t.data.data, ["list"])
     list.value = t.data.data?.list
   })
 }
-const handleCurrentChange = (val: number) => {
-  handleSearch({...page.value, page: val})
+const handlePageIndexChange = (val: number) => {
+  page.value.page = val
+  handleSearch()
 }
-const handleSizeChange = (val: number) => {
-  handleSearch({...page.value, pageSize: val})
+const handlePageSizeChange = (val: number) => {
+  page.value.pageSize = val
+  handleSearch()
 }
 onMounted(() => {
   handleSearch()
