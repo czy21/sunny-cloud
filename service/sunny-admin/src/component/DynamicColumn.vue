e7aaa2d551335b14ed31dc97154bb07ce496e846
Bruce Chen <805899926@qq.com>
2024-11-03 01:58:33 +0800

    # input ref

--- service/sunny-admin/src/component/DynamicColumn.vue
+++ service/sunny-admin/src/component/DynamicColumn.vue
@@ -9,27 +9,37 @@
   <el-table-column :label="props.node.desc" :prop="props.node.name" align="center" v-else>
     <template #default="scope">
       <slot :columnName="props.node.name" :column="node" :scope="scope" v-if="props.node.custom"/>
-      <el-input v-model="scope.row[props.node.name]" @blur="onExitEditMode(scope)" v-else-if="isInputString(scope)"/>
-      <el-input v-model="scope.row[props.node.name]" type="number" @blur="onExitEditMode(scope)" v-else-if="isInputNumber(scope)"/>
+      <el-input ref="editRef" v-model="scope.row[props.node.name]" @blur="onExitEditMode(scope)" v-else-if="isInputString(scope)"/>
+      <el-input ref="editRef" v-model="scope.row[props.node.name]" type="number" @blur="onExitEditMode(scope)" v-else-if="isInputNumber(scope)"/>
     </template>
   </el-table-column>
 </template>
 
 <script setup lang="tsx">
-import {defineProps} from "vue"
+import {ref, defineProps} from "vue"
 
 const props = defineProps(["node"])
+
+const editRef = ref(null)
 
 const isEditable = (scope) => {
   return props.node.editable && scope.row[`${props.node.name}_editable`]
 }
 
 const isInputString = (scope) => {
-  return (props.node.type === 'string' || !props.node.type) && isEditable(scope)
+  let val = (props.node.type === 'string' || !props.node.type) && isEditable(scope)
+  if (val) {
+    editRef.value?.focus()
+  }
+  return val
 }
 
 const isInputNumber = (scope) => {
-  return props.node.type === 'number' && isEditable(scope)
+  let val = props.node.type === 'number' && isEditable(scope)
+  if (val) {
+    editRef.value?.focus()
+  }
+  return val
 }
 
 const onExitEditMode = (scope) => (scope.row[`${props.node.name}_editable`] = null)
