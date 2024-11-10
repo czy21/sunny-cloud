36b2220c4f690b241b4ebe63812c45270a84762c
Bruce Chen <805899926@qq.com>
2024-11-10 22:36:57 +0800

    #

---
+++ global/sunny-framework-js/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(cd "$(dirname "$0")"; pwd)
+registry=http://nexus.czy21-internal.com/repository/npm-hosted/
+
+
+for pkg in `ls -d packages/*`;do
+  rm -rf $pkg/dist
+  pkg_name=$(basename $pkg)
+  npm run build -w @sunny-framework-js/$pkg_name
+  if [ "$1" == "--deploy" ];then
+    npm publish --registry $registry -w @sunny-framework-js/$pkg_name
+  fi
+done
--- global/sunny-framework-js/shared/rollup.config.mjs
+++ global/sunny-framework-js/shared/rollup.config.mjs
@@ -1,47 +1,50 @@
 import path from 'path'
-import { builtinModules } from 'module';
+import {readFileSync} from 'fs';
+import {builtinModules} from 'module';
 import typescript from '@rollup/plugin-typescript';
 
 /**
  * Create a base rollup config
  * @param {Record<string,any>} pkg Imported package.json
  * @param {string[]} external Imported package.json
- * @returns {import('rollup').RollupOptions}
+ * @returns {import('rollup').RollupOptions || import('rollup').RollupOptions[]}
  */
-export function createConfig({ pkg, external = [] }) {
-  external.push(/node_modules/)
-  if (pkg.name !== '@sunny-framework-js/util'){
-    external.push("@sunny-framework-js/util")
-    external.push("axios")
-    external.push("lodash")
-  }
-  return {
-    input: 'src/index.ts',
-    external: Object.keys(pkg.dependencies || {})
-      .concat(Object.keys(pkg.peerDependencies || {}))
-      .concat(builtinModules)
-      .concat(external),
-    onwarn: (warning) => {
-      throw Object.assign(new Error(), warning);
-    },
-    output: [
-      {
-        format: 'cjs',
-        dir: path.resolve(pkg.main,'..'),
-        sourcemap: true,
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        exports: "named"
-      },
-      {
-        format: 'es',
-        dir: path.resolve(pkg.module,'..'),
-        sourcemap: true,
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        exports: "named"
-      }
-    ],
-    plugins: [typescript({ sourceMap: true })]
-  };
+export function createConfig({pkg, external = []}) {
+
+    if (pkg.name !== '@sunny-framework-js/util') {
+        let util_pkg = JSON.parse(readFileSync(new URL('../packages/util/package.json', import.meta.url), 'utf8'))
+        external.push(util_pkg.name)
+        external = external.concat(Object.keys(util_pkg.dependencies || {})).concat(Object.keys(util_pkg.peerDependencies || {}))
+    }
+
+    let builds = [
+        {
+            "format": "cjs",
+            "dir": path.resolve(pkg.main, '..'),
+        },
+        {
+            "format": "es",
+            "dir": path.resolve(pkg.module, '..')
+        }
+    ]
+
+    return builds.map(t => {
+        return {
+            input: 'src/index.ts',
+            external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})).concat(builtinModules).concat(external),
+            onwarn: (warning) => {
+                throw Object.assign(new Error(), warning);
+            },
+            output: [
+                {
+                    ...t,
+                    sourcemap: true,
+                    preserveModules: true,
+                    preserveModulesRoot: "src",
+                    exports: "named",
+                }
+            ],
+            plugins: [typescript({sourceMap: true, declaration: true, declarationDir: t.dir})]
+        }
+    })
 }
