0c15a22eb688e809ae3586c58b53d97bdac455a6
Bruce Chen <805899926@qq.com>
2024-11-14 21:50:04 +0800

    # adjust export

--- global/sunny-framework-js/packages/util/index.ts
+++ global/sunny-framework-js/packages/util/index.ts
@@ -1,4 +1,11 @@
-export * as basic from './src/basic'
-export * as auth from './src/auth'
-export * as object from './src/object'
-export * as tree from './src/tree'
+import * as basic from './src/basic'
+import * as auth from './src/auth'
+import * as object from './src/object'
+import * as tree from './src/tree'
+
+export default {
+    basic,
+    auth,
+    object,
+    tree
+}
