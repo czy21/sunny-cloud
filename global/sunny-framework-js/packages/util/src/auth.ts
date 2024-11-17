86fd3a853b39f1f9859626e83fb2b5c85f7ef5b7
Bruce Chen <805899926@qq.com>
2024-11-17 23:37:00 +0800

    # adjust version

--- global/sunny-framework-js/packages/util/src/auth.ts
+++ global/sunny-framework-js/packages/util/src/auth.ts
@@ -1,13 +1,13 @@
 import Cookies, {CookieAttributes} from "js-cookie";
 
-export const getToken = (tokenKey = "token") => {
+export const getToken = (tokenKey?: string = "token") => {
     return Cookies.get(tokenKey)
 }
 
-export const setToken = (tokenKey = "token", value: string, options?: CookieAttributes) => {
+export const setToken = (value: string, options?: CookieAttributes, tokenKey?: string = "token") => {
     Cookies.set(tokenKey, value, {domain: '.' + window.location.hostname.split('.').slice(-2).join('.'), ...options});
 }
 
-export const delToken = (tokenKey = "token", options?: CookieAttributes) => {
+export const delToken = (options?: CookieAttributes, tokenKey?: string = "token") => {
     Cookies.remove(tokenKey, {domain: '.' + window.location.hostname.split('.').slice(-2).join('.'), ...options});
 }
