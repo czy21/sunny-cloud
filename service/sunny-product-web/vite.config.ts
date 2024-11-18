1f28f3832c7af39fe32380d38498c4d7ea4bd8c8
Bruce CHEN <805899926@qq.com>
2024-11-18 11:12:38 +0800

    # add product web for react

---
+++ service/sunny-product-web/vite.config.ts
@@ -0,0 +1,26 @@
+import {fileURLToPath, URL} from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {resolveFrameworkImporter} from "../../global/sunny-framework-js/plugin"
+
+const alias = {
+  "@": "src",
+  "@v": "src/view",
+  "@c": "src/component"
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => /node_modules|sunny-framework-js/.test(id) ? "vendor" : null
+      },
+    }
+  },
+  resolve: {
+    alias: Object.keys(alias).reduce((p, c) => ({...p, [c]: fileURLToPath(new URL(alias[c], import.meta.url))}), {})
+  },
+})
