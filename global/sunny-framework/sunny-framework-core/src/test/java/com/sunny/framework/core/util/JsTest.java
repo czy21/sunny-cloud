71266603b2911b7c19a1d3d0f0321bcb57de5bdc
Bruce Chen <805899926@qq.com>
2024-12-28 21:35:07 +0800

    # add js test

--- global/sunny-framework/sunny-framework-core/src/test/java/com/sunny/framework/core/util/JsTest.java
+++ global/sunny-framework/sunny-framework-core/src/test/java/com/sunny/framework/core/util/JsTest.java
@@ -1,11 +1,13 @@
 package com.sunny.framework.core.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.IntegerRange;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class JsTest {
 
@@ -13,11 +15,13 @@ public class JsTest {
 
     @Test
     public void test1() {
-        Map<String, Object> row = new HashMap<>(Map.of(
-                "query", new HashMap<>(Map.of("year", 2024)),
-                "name", "world"
-        ));
-        Integer val = EngineUtil.getValue(row, "obj.query.year", Integer.class);
-        Assert.isTrue(val == 2024, "year should be 2024");
+        Map<String, Object> row = new HashMap<>();
+        row.put("query", Map.of("year", 2024));
+        row.put("name", "john");
+        IntStream.rangeClosed(1, 10).forEach(i -> row.put("m" + i, 1));
+        Integer yearValue = ScriptUtil.getJsValue(row, "obj.query?.year", Integer.class);
+        Integer yearTotal = ScriptUtil.getJsValue(row, "Array.from({length:12}).map((t,i,a)=>Number(obj[`m${i+1}`]) || 0).reduce((a, b) => a + b)", Integer.class);
+        Assert.isTrue(yearValue == 2024, "year should be 2024");
+        Assert.isTrue(yearTotal == 10, "yearTotal should be 10");
     }
 }
