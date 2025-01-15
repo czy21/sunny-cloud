30baea80ae6c62c53c3d6614d84667ee0a010ba3
Bruce Chen <805899926@qq.com>
2025-01-15 20:58:11 +0800

    #

--- global/sunny-framework-js/packages/vue/src/component/table/DynamicNaiveTable.vue
+++ global/sunny-framework-js/packages/vue/src/component/table/DynamicNaiveTable.vue
@@ -1,28 +1,24 @@
 <template>
-
-  <n-data-table ref="tableRef"
-                :columns="columns"
-                :data="props.data"
-                :max-height="500"
-                :scroll-x="1800"
-                virtual-scroll
-                :single-line="false"
-                size="small"
-                striped
-                :render-cell="(value, rowData, column)=>{console.log(column);return value}"
-  >
-
-  </n-data-table>
+  <n-config-provider>
+    <n-data-table ref="tableRef"
+                  :columns="columns"
+                  :data="props.data"
+                  :max-height="500"
+                  :scroll-x="1800"
+                  virtual-scroll
+                  :single-line="false"
+                  size="small"
+                  :render-cell="(value,row,column)=>handleRenderCell({row,column})"
+    />
+  </n-config-provider>
 </template>
 
 <script setup lang="tsx">
-import {FunctionalComponent, ref, computed} from "vue"
-import DynamicVxeColumn from "./DynamicVxeColumn.vue"
+import {FunctionalComponent, ref, computed, h} from "vue"
 import util from '@sunny-framework-js/util'
 import {TableProps, TableEmits} from "./DynamicTable.ts";
 import {NDataTable} from "naive-ui";
 import {ElButton, ElDatePicker, ElInput, ElInputNumber, ElOption, ElSelect} from "element-plus";
-import {it} from "node:test";
 import _ from "lodash";
 
 const props = withDefaults(defineProps<TableProps>(), {
@@ -54,11 +50,21 @@ const props = withDefaults(defineProps<TableProps>(), {
 function recursiveColumn(node: any) {
   const newNode: any = {
     key: node.prop,
+    property: node.prop,
     title: node.name,
-    width: node.style?.width || node.width || 150,
     titleAlign: "center",
+    resizable: true,
+    minWidth: 100,
+    width: node.style?.width || node.width || 150,
+    fixed: node.fixed,
     params: _.omit(node, ["children"])
-  };
+  }
+  if (newNode.params.type === 'index') {
+    newNode.render = (row, rowIndex) => {
+      row[newNode.property] = rowIndex + 1
+      return row[newNode.property]
+    }
+  }
   for (let child of (node.children || [])) {
     newNode.children = newNode.children || []
     newNode.children.push(recursiveColumn(child));
@@ -68,8 +74,30 @@ function recursiveColumn(node: any) {
 
 let columns = computed(() => recursiveColumn({children: props.columns}).children)
 
+
+const handleRenderCell = (scope) => {
+  let label = scope.row[scope.column.property]
+  if (scope.column.params.dictKey && props.dict) {
+    const options = props.dict[scope.column.params.dictKey]
+    let value = options?.find(t => t.value === scope.row[scope.column.property])?.label
+    if (value) {
+      label = value
+    }
+    // if (!label && !value && options?.length === 1 && scope.column.params.dictOnlyOneDefaultSelect) {
+    //   scope.row[scope.column.property] = options[0].value
+    //   handleExtra(scope.row[scope.column.property], scope)
+    // }
+  }
+  if (scope.column.params.type === 'number') {
+    label = util.number.toMilliSeparator(label, true, !util.object.isEmpty(scope.column.params.precision) ? scope.column.params.precision : 2)
+  }
+  return label
+}
+
 </script>
 
 <style scoped lang="scss">
-
+:deep(.n-data-table .n-data-table-td.n-data-table-td--last-row) {
+  border-bottom: 1px solid var(--n-merged-border-color);
+}
 </style>
