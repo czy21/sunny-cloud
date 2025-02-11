1e47d617714d2f5a86d11e986259833b9744027c
Bruce Chen <805899926@qq.com>
2025-02-11 17:22:53 +0800

    # adjust undertom websocket dep

--- service/sunny-system/sunny-system-core/src/main/java/com/sunny/system/core/config/SocketConfigure.java
+++ service/sunny-system/sunny-system-core/src/main/java/com/sunny/system/core/config/SocketConfigure.java
@@ -1,6 +1,5 @@
 package com.sunny.system.core.config;
 
-import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -10,7 +9,6 @@ public class SocketConfigure {
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
-        ServerEndpointExporter endpointExporter= new ServerEndpointExporter();
-        return endpointExporter;
+        return new ServerEndpointExporter();
     }
 }
