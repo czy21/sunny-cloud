b2d716cb2f1f2e6c94d78544d3c6d9f7ff856163
Bruce Chen <805899926@qq.com>
2025-02-12 15:46:51 +0800

    #

--- service/sunny-admin/src/view/chat/Simple.vue
+++ service/sunny-admin/src/view/chat/Simple.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-button @click="simpleConnect" :type="status=='success'?'danger':''">{{ status == 'success' ? '断开' : '连接' }}</el-button>
+  <el-button @click="onConnect" :type="status=='success'?'danger':''">{{ status == 'success' ? '断开' : '连接' }}</el-button>
   <el-button @click="messages=[]">清空</el-button>
   <p v-for="t in messages">
     {{ t }}
@@ -18,10 +18,7 @@ const socketObj = ref(new socket.web.Socket({
   server: "ws://127.0.0.1:8080/socket/simple/1"
 }))
 
-const sockjsConnectStatuColor = ref()
-const sockjsMessages = ref([])
-
-const simpleConnect = () => {
+const onConnect = () => {
   if (status.value == "success") {
 
     socketObj.value.onclose = ev => {
