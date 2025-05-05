d194833cf1c14495189cdd97425a46a455c69fb2
Bruce Chen <805899926@qq.com>
2025-05-05 15:20:13 +0800

    # add WebConfigure.cs

---
+++ global/Sunny.Framework/Sunny.Framework.Web/WebConfigure.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nacos.V2.DependencyInjection;
+using NLog.Web;
+
+namespace Sunny.Framework.Web
+{
+    public static class WebConfigure
+    {
+        public static IHostBuilder UseWebConfigure(this IHostBuilder builder)
+        {
+            builder.UseNacosConfig("NacosConfig");
+
+            builder.ConfigureLogging(c => c.ClearProviders()).UseNLog();
+
+            return builder;
+        }
+
+        public static IServiceCollection AddWebConfigure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddNacosV2Config(config, null, "NacosConfig");
+            return services;
+        }
+    }
+}
